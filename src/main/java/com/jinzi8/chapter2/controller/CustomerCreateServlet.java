489e60775a1fc2c3faa7429d00f6a782f1eeace7
jinzi8 <jzzswb@163.com>
2019-03-14 16:25:30 +0800

    创建chapter2工程

---
+++ src/main/java/com/jinzi8/chapter2/controller/CustomerCreateServlet.java
@@ -0,0 +1,28 @@
+package com.jinzi8.chapter2.controller;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+@WebServlet("/customer_create")
+public class CustomerCreateServlet extends javax.servlet.http.HttpServlet {
+    /**
+     * 进入创建客户界面
+     * @param request
+     * @param response
+     * @throws javax.servlet.ServletException
+     * @throws IOException
+     */
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    //TODO
+    }
+
+    /**
+     * 处理创建客户请求
+     * @param request
+     * @param response
+     * @throws javax.servlet.ServletException
+     * @throws IOException
+     */
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        //TODO
+    }
+}
